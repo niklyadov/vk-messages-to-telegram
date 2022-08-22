8a2cdf56c0480f70b0623be33900bb9fb84a6727
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2022-08-22 22:02:31 +0500

    Set absolute hosted path

--- host-windows-service.bat
+++ host-windows-service.bat
@@ -1,6 +1,6 @@
 set serviceName=VkToTg
-set hostedPath=%cd%\Hosted
-@REM set hostedPath=C:\Hosted
+@REM set hostedPath=%cd%\Hosted
+set hostedPath=C:\Hosted
 set servicePath=%hostedPath%\%serviceName%
 
 net stop %serviceName%
@@ -11,7 +11,7 @@ dotnet publish -c Release --no-self-contained --output %servicePath% %serviceNam
 sc.exe delete %serviceName%
 sc.exe create %serviceName% binPath="%servicePath%\%serviceName%.exe"
 
-@REM copy /y "%hostedPath%\%serviceName%-config.json" "%servicePath%\appsettings.json"
+copy /y "%hostedPath%\%serviceName%-config.json" "%servicePath%\appsettings.json"
 
 net start %serviceName%
 pause
